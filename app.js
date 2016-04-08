e8efa07912b3cb6dc3caedbc05b8ff31e26132dd
aamer mohammed <aamermail@gmail.com>
2016-04-08 18:11:34 +0530

    [#316 - srikanth/aamer] Filtering logic for reports based on period

--- app.js
+++ app.js
@@ -7,48 +7,62 @@ else
 var ApiUrl = dhisUrl + '/api';
 
 Reports.controller('ReportsController',['UserService', 'DataSetService', '$scope', 'DataVizObjectService', 'Config', function(userService, DataSetService, $scope, DataVizObjectService, Config) {
-    var user;
-    $scope.mmrDataSet = {}
-    $scope.mmrDataVizObjects = []
-    var result = {}
-    $scope.months=['January', 'February', 'March', 'April' ];
-    $scope.years= [];
-    $scope.month=null;
-    $scope.year=null;
-
-    $scope.isShow=false;
     $scope.getReport = function(){
-        console.log($scope.month)
-        console.log($scope.year)
+        $scope.user = null;
+        $scope.mmrDataSet = {}
+        $scope.mmrDataVizObjects = []
+        $scope.mmrFilteredDataVizObjects = []
+        var result = {}
+        $scope.months=['January', 'February', 'March', 'April' ];
+        $scope.years= [];
+        $scope.month=null;
+        $scope.year=null;
+        $scope.isShow=false;
         $scope.isShow=true;
+
+        var getMMRDataSet = function(dataSets) {
+            return _.filter(dataSets, function(dataSet) {
+                return dataSet.name.startsWith(Config.dataSetObjectName)
+            })[ 0 ];
+        };
+
+        var getDataVizObjects = function(user) {
+            $scope.user = user;
+            return DataVizObjectService.getDataVizObjects(user)
+              .then(function(dataVizObjects) {
+                      $scope.mmrDataVizObjects = dataVizObjects;
+                  })
+        };
+
+        var filterDataVizObjects = function() {
+            return DataVizObjectService.getFilteredDataVizObjects($scope.mmrDataVizObjects, '201604')
+              .then(function(dataVizObjects) {
+                  $scope.mmrFilteredDataVizObjects = dataVizObjects;
+              });
+        }
+
+        var assignFilteredDataVizObjects = function() {
+            if($scope.mmrFilteredDataVizObjects.length!=0)
+                return DataSetService.getDataSet(getMMRDataSet($scope.user.project.dataSets).id, $scope.mmrFilteredDataVizObjects)
+                  .then(function(dataset) {
+                      console.log(dataset)
+                      return $scope.mmrDataSet = dataset;
+                  });
+            else
+                alert('No data charts configured for selected period');
+        }
+
+        userService.getLoggedInUser()
+          .then(getDataVizObjects)
+          .then(filterDataVizObjects)
+          .then(assignFilteredDataVizObjects)
+
+
+
+
+        for(var i= new Date().getFullYear() ;i>=2000;i--) {
+            $scope.years.push(i);
+        }
     };
 
-    for(var i= new Date().getFullYear() ;i>=2000;i--) {
-        $scope.years.push(i);
-    }
-
-    var getMMRDataSet = function(dataSets) {
-        return _.filter(dataSets, function(dataSet) {
-            return dataSet.name.startsWith(Config.dataSetObjectName)
-        })[ 0 ];
-    };
-
-    userService.getLoggedInUser()
-      .then(function(user) {
-          $scope.user = user;
-          DataVizObjectService.getDataVizObjects(user)
-            .then(function(dataVizObjects) {
-                _.map(dataVizObjects, function(map) {
-                    //console.log(map);
-                    $scope.mmrDataVizObjects.push(map);
-                    //console.log(map.name, dataVizObjects.length);
-                })
-            });
-
-          DataSetService.getDataSet(getMMRDataSet(user.project.dataSets).id, $scope.mmrDataVizObjects)
-            .then(function(dataset) {
-                console.log(dataset);
-                return $scope.mmrDataSet = dataset;
-            });
-      });
-    } ]);
+} ]);
--- services/DataVizObjectsService.js
+++ services/DataVizObjectsService.js
@@ -1,10 +1,10 @@
 Reports.service('DataVizObjectService',['$http','Config', function($http, config){
+    var failurePromise = function(){
+        alert('Fetching data failed');
+    };
+
     this.getDataVizObjects = function(user){
         var dataVizObjects=[];
-        var failurePromise = function(){
-            alert('Fetching data failed');
-        };
-
 
         var getCharts = function(){
             var chartsSuccessPromise = function(response){
@@ -26,10 +26,31 @@ Reports.service('DataVizObjectService',['$http','Config', function($http, config
         var promises = [];
         promises.push(getCharts());
         promises.push(getReportTables());
+        dataVizObjects.isResolved = Promise.all(promises);
         return Promise.all(promises)
             .then(function(){
                 return _.flatten(dataVizObjects);
             });
 
     };
+
+    this.getFilteredDataVizObjects = function(dataVizObjects, date) {
+        var filteredDataVizObjects  = []
+        var filterData = function() {
+            var filterSuccessPromise = function(response) {
+                if( !_.isEmpty(response.data) )
+                    filteredDataVizObjects.push(response.data);
+            };
+            return Promise.all(_.map(dataVizObjects, function(map) {
+                return $http.get(map.href.replace("8080","8000") + "?filter=periods.code:eq:" + date)
+                  .then(filterSuccessPromise, failurePromise);
+            }));
+        };
+
+        return filterData()
+          .then(function(){
+              return _.flatten(filteredDataVizObjects);
+          });
+    };
+
 }]);
