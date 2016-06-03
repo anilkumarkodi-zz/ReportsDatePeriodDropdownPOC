614a33a914005c3afda73e8797f7cf3ef610a927
durgaman <durgaman@INdurgaman.local>
2016-06-03 20:30:47 +0530

    #335 [Sultan/Durga/Sayan] added unit tests for viz object service and changed the two if branches in viz object service

--- services/VizObjectService.js
+++ services/VizObjectService.js
@@ -2,10 +2,8 @@ Reports.service('VizObjectService', ['$http', 'Config', function ($http, config)
     var failurePromise = function () {
         alert('Fetching data failed');
     };
-
     this.getVizObjects = function (user, dataSetCode, selectedTimePeriod) {
         var vizObjects = [];
-
         var getCharts = function () {
             var chartsSuccessPromise = function (response) {
                 vizObjects.push(response.data.charts)
@@ -13,16 +11,13 @@ Reports.service('VizObjectService', ['$http', 'Config', function ($http, config)
             return $http.get(ApiUrl + "/charts.json?filter=name:ilike:" + dataSetCode)
                 .then(chartsSuccessPromise, failurePromise);
         };
-
         var getReportTables = function () {
             var reportTablesSuccessPromise = function (response) {
                 vizObjects.push(response.data.reportTables)
             };
-
             return $http.get(ApiUrl + "/reportTables.json?filter=name:ilike:" + dataSetCode)
                 .then(reportTablesSuccessPromise, failurePromise);
         };
-
         var getEventCharts = function () {
             var eventChartsSuccessPromise = function (response) {
                 vizObjects.push(response.data.eventCharts)
@@ -30,16 +25,13 @@ Reports.service('VizObjectService', ['$http', 'Config', function ($http, config)
             return $http.get(ApiUrl + "/eventCharts.json?filter=name:ilike:" + dataSetCode)
                 .then(eventChartsSuccessPromise, failurePromise);
         };
-
         var getEventReportTables = function () {
             var eventReportTablesSuccessPromise = function (response) {
                 vizObjects.push(response.data.eventReports)
             };
-
             return $http.get(ApiUrl + "/eventReports.json?filter=name:ilike:" + dataSetCode)
                 .then(eventReportTablesSuccessPromise, failurePromise);
         };
-
         var calculateReportTableTotal = function (data) {
             var visibleHeaderIndex = [];
             var headers = data.headers;
@@ -58,7 +50,6 @@ Reports.service('VizObjectService', ['$http', 'Config', function ($http, config)
                 newRow[visibleHeaderIndex] = (columnSum == 0) ? "" : columnSum;
             });
             rows.push(newRow);
-
             _.map(rows, function (row) {
                 var rowSum = 0;
                 _.map(visibleHeaderIndex, function (visibleHeaderIndex) {
@@ -67,9 +58,8 @@ Reports.service('VizObjectService', ['$http', 'Config', function ($http, config)
                 row[headers.length - 1] = (rowSum == 0) ? "" : rowSum;
             })
         };
-
         var getReportTablesJson = function (vizObjects) {
-            _.map(vizObjects, function (dataObject, index) {
+            var promises = _.map(vizObjects, function (dataObject, index) {
                 dataObject.jsonData = {};
                 var reportTablesJsonSuccessPromise = function (response) {
                     if ((vizObjects[index].href).indexOf("charts") > -1)
@@ -80,28 +70,30 @@ Reports.service('VizObjectService', ['$http', 'Config', function ($http, config)
                     }
                 };
                 var dataObjectUrl = ApiUrl;
-                if ((dataObject.href).indexOf("charts") > -1)
+                if ((dataObject.href).indexOf("charts") > -1) {
                     dataObjectUrl = dataObjectUrl + "/charts/" + dataObject.id + "/data.json";
-                if ((dataObject.href).indexOf("reportTables") > -1)
+                    return $http.get(dataObjectUrl)
+                        .then(reportTablesJsonSuccessPromise, failurePromise);
+                }
+                if ((dataObject.href).indexOf("reportTables") > -1) {
                     dataObjectUrl = dataObjectUrl + "/reportTables/" + dataObject.id + "/data.json?date=" + selectedTimePeriod;
-
-                return $http.get(dataObjectUrl)
-                    .then(reportTablesJsonSuccessPromise, failurePromise);
+                    return $http.get(dataObjectUrl)
+                        .then(reportTablesJsonSuccessPromise, failurePromise);
+                }
             });
+            vizObjects.isResolved = promises;
             return vizObjects;
         };
-
         var promises = [];
         promises.push(getCharts());
         promises.push(getReportTables());
         promises.push(getEventCharts());
         promises.push(getEventReportTables());
-        vizObjects.isResolved = Promise.all(promises);
-        return Promise.all(promises)
+        vizObjects.isResolved = Promise.all(promises)
             .then(function () {
                 return _.flatten(vizObjects);
             })
             .then(getReportTablesJson);
-
+        return vizObjects;
     };
 }]);
--- tests/services/VizObjectServiceSpec.js
+++ tests/services/VizObjectServiceSpec.js
@@ -1,4 +1,4 @@
-xdescribe("VizObjectService", function () {
+describe("VizObjectService", function () {
     var vizObjectService;
     var httpMock;
     var $rootScope;
@@ -26,88 +26,106 @@ xdescribe("VizObjectService", function () {
             var serverData = {
                 charts: [{
                     id: "1",
-                    displayName: "REP_MMR02_TEST_CHART1",
+                    displayName: "test chart",
                     href: "http://localhost:8000/api/charts"
                 }],
-
                 reportTables: [{
                     id: "12",
-                    displayName: "REP_MMR01_TEST_REPORT_TABLE1",
-                    href: "http://localhost:8000/api/reportTables"
+                    displayName: "test report table",
+                    href: "http://localhost:8000/api/reportTables",
                 }],
-
                 eventCharts: [{
                     id: "123",
-                    displayName: "REP_MMR02_TEST_CHART1",
-                    href: "http://localhost:8000/api/eventCharts"
+                    displayName: "test event chart",
+                    href: "http://localhost:8000/api/eventCharts",
                 }],
-
                 eventReports: [{
                     id: "1234",
-                    displayName: "REP_MMR02_TEST_CHART1",
-                    href: "http://localhost:8000/api/evenetReports"
-                }]
+                    displayName: "test event report",
+                    href: "http://localhost:8000/api/eventReports"
+                }],
+                chartsData: {
+                    id: '1',
+                    displayName: 'test chart',
+                    href: 'http://localhost:8000/api/charts',
+                    jsonData: 'http://localhost:8000/api/charts/data'
+                },
+                reportTableData: {
+                    id: '12',
+                    displayName: 'test report table',
+                    href: 'http://localhost:8000/api/reportTables',
+                    jsonData: 'http://localhost:8000/api/reportTables/data',
+                    headers: [{hidden: false, name: "Total"}],
+                    rows: [{}]
+                }
             };
 
             var expectedVizObjects = [{
-                    id: '1',
-                    displayName: 'REP_MMR02_TEST_CHART1',
-                    href: 'http://localhost:8000/api/charts',
-                    jsonData: 'http://localhost:8000/api/charts/data'
-                }, {
-                    id: '12',
-                    displayName: 'REP_MMR01_TEST_REPORT_TABLE1',
+                id: '1',
+                displayName: 'test chart',
+                href: 'http://localhost:8000/api/charts',
+                jsonData: 'http://localhost:8000/api/charts/data'
+            }, {
+                id: '12',
+                displayName: 'test report table',
+                href: 'http://localhost:8000/api/reportTables',
+                jsonData: {
+                    id: "12",
+                    displayName: 'test report table',
                     href: 'http://localhost:8000/api/reportTables',
-                    jsonData: 'http://localhost:8000/api/reportTables/data'
-                }, {
-                    id: '123',
-                    displayName: 'REP_MMR02_TEST_CHART1',
-                    href: 'http://localhost:8000/api/eventCharts',
-                    jsonData: {}
-                }, {
-                    id: '1234',
-                    displayName: 'REP_MMR02_TEST_CHART1',
-                    href: 'http://localhost:8000/api/evenetReports',
-                    jsonData: {}
-                }]
-                ;
+                    jsonData: 'http://localhost:8000/api/reportTables/data',
+                    headers: [{hidden: false, name: "Total"}, {hidden: false, name: "Total"}],
+                    rows: [{1: NaN}, [NaN, NaN]]
+                }
+            }, {
+                id: '123',
+                displayName: 'test event chart',
+                href: 'http://localhost:8000/api/eventCharts',
+                jsonData: {}
+            }, {
+                id: '1234',
+                displayName: 'test event report',
+                href: 'http://localhost:8000/api/eventReports',
+                jsonData: {}
+            }];
 
             httpMock.expectGET("http://localhost:8000/api/charts.json?filter=name:ilike:" + dataSetCode).respond(200, serverData);
             httpMock.expectGET("http://localhost:8000/api/reportTables.json?filter=name:ilike:" + dataSetCode).respond(200, serverData);
             httpMock.expectGET("http://localhost:8000/api/eventCharts.json?filter=name:ilike:" + dataSetCode).respond(200, serverData);
             httpMock.expectGET("http://localhost:8000/api/eventReports.json?filter=name:ilike:" + dataSetCode).respond(200, serverData);
 
-            vizObjectService.getVizObjects(user, dataSetCode, selectedTimePeriod).then(function (actualVizObjects) {
-                console.log('hello there')
-                httpMock.expectGET("http://localhost:8000/api/charts/1/data.json").respond(200, expectedVizObjects[0]);
-                httpMock.expectGET("http://localhost:8000/api/reportTables/12/data.json?date=" + selectedTimePeriod).respond(200, expectedVizObjects[1]);
-                httpMock.expectGET("http://localhost:8000/api" + selectedTimePeriod).respond(200, expectedVizObjects[2]);
-                httpMock.expectGET("http://localhost:8000/api" + selectedTimePeriod).respond(200, expectedVizObjects[3]);
-                console.log(actualVizObjects)
-                expect(expectedVizObjects).toEqual(actualVizObjects);
+            vizObjectService.getVizObjects(user, dataSetCode, selectedTimePeriod).isResolved.then(function (actualVizObjects) {
+                httpMock.expectGET("http://localhost:8000/api/charts/1/data.json").respond(200, serverData.chartsData);
+                httpMock.expectGET("http://localhost:8000/api/reportTables/12/data.json?date=" + selectedTimePeriod).respond(200, serverData.reportTableData);
+                httpMock.flush(1);
+                httpMock.flush(1);
+                expectedVizObjects.isResolved = actualVizObjects.isResolved;
+                expect(actualVizObjects).toEqual(expectedVizObjects);
                 done();
             });
-            httpMock.flush();
-            console.log('digested')
+            httpMock.flush(4);
             setTimeout($rootScope.$digest, 900);
-
         });
 
         it("should get the failure promise when server not responded", function () {
             var dataSetCode = "123";
             var selectedTimePeriod = "26-05-2016";
             var user = {};
-
             spyOn(window, 'alert');
+
             httpMock.expectGET("http://localhost:8000/api/charts.json?filter=name:ilike:" + dataSetCode).respond(404);
             httpMock.expectGET("http://localhost:8000/api/reportTables.json?filter=name:ilike:" + dataSetCode).respond(404);
             httpMock.expectGET("http://localhost:8000/api/eventCharts.json?filter=name:ilike:" + dataSetCode).respond(404);
             httpMock.expectGET("http://localhost:8000/api/eventReports.json?filter=name:ilike:" + dataSetCode).respond(404);
 
-            vizObjectService.getVizObjects(user, dataSetCode, selectedTimePeriod).then(function () {
-                expect(window.alert).toHaveBeenCalledWith("Fetching data failed");
+            vizObjectService.getVizObjects(user, dataSetCode, selectedTimePeriod).isResolved.then(function () {
+                httpMock.expectGET("http://localhost:8000/api/charts/1/data.json").respond(404);
+                httpMock.expectGET("http://localhost:8000/api/reportTables/12/data.json?date=" + selectedTimePeriod).respond(404);
+                expect(window.alert).toHaveBeenCalledWith("");
+                done();
             });
-            httpMock.flush();
+            httpMock.flush(4);
+            setTimeout($rootScope.$digest, 900);
         })
     });
 });
